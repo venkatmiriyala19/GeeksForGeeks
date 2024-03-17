deb589fa6090c59cf9aa93023ca5320dfc589a32
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-03-17 12:17:21 +0530

    Create  Check If Circular Linked List.java

---
+++ Check If Circular Linked List.java
@@ -0,0 +1,17 @@
+class GfG
+{
+    boolean isCircular(Node head)
+    {
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if (slow==fast){
+                return true;
+            }
+        }
+        return false;
+	// Your code here	
+    }
+}
