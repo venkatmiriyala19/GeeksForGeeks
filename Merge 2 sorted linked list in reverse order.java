5bc53660670aea384fd4d988de3a470adfd61d9b
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-04-04 14:51:51 +0530

    Create Merge 2 sorted linked list in reverse order.java

---
+++ Merge 2 sorted linked list in reverse order.java
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t=scanner.nextInt();
+        while(t-->0)
+        {
+            int N = scanner.nextInt();
+            int M = scanner.nextInt();
+
+            Node node1 = null;
+            Node temp1 = null;
+            for (int i = 0; i < N; i++) {
+                int value = scanner.nextInt();
+                Node newNode = new Node(value);
+                if (node1 == null) {
+                    node1 = newNode;
+                    temp1 = node1;
+                } else {
+                    temp1.next = newNode;
+                    temp1 = temp1.next;
+                }
+            }
+    
+            Node node2 = null;
+            Node temp2 = null;
+            for (int i = 0; i < M; i++) {
+                int value = scanner.nextInt();
+                Node newNode = new Node(value);
+                if (node2 == null) {
+                    node2 = newNode;
+                    temp2 = node2;
+                } else {
+                    temp2.next = newNode;
+                    temp2 = temp2.next;
+                }
+            }
+    
+            GfG gfg = new GfG();
+            Node result = gfg.mergeResult(node1, node2);
+    
+            printList(result);
+        }
+    }
+
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/* Structure of the node*/
+/* class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+} */
+
+class GfG
+{
+    Node mergeResult(Node node1, Node node2)
+    {
+        if (node1==null && node2==null){
+            return null;
+        }
+        if (node1==null){
+            node1=node2;
+            node2=null;
+        }
+        
+        ArrayList<Integer> list= new ArrayList<>();
+        Node current=node1;
+        while (current.next!=null){
+            current=current.next;
+        }
+        current.next=node2;
+        current=node1;
+    
+        while(current!=null){
+                list.add(current.data);
+                current=current.next;
+            }
+        Collections.sort(list);
+        current=node1;
+        for (int i=list.size()-1;i>=0;i--){
+            current.data=list.get(i);
+            current=current.next;
+        }
+        return node1;
+	// Your code here	
+    }
+}
