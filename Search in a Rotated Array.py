5fba41a4cee52a3b1ef40dd9cd29b971a29b893e
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-04-15 11:06:38 +0530

    Create Search in a Rotated Array.py

---
+++ Search in a Rotated Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def search(self, A : list, l : int, h : int, key : int):
+        # l: The starting index
+        # h: The ending index, you have to search the key in this range
+        # Complete this function
+        if key in A:
+            return A.index(key)
+        return -1
