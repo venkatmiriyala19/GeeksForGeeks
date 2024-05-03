cb4bd8250f6d1ee2df91abcae96014e79f9c3365
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-03 17:14:45 +0530

    Create The New Line - Java.java

---
+++ The New Line - Java.java
@@ -0,0 +1,13 @@
+class Geeks{
+    
+    // Function to print each word in single line
+    static void printIndividualLine(){
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+        
+        // Your code here
+        
+    }
+    
+}
