4a7dcbdbf10fd28acc6b2d660e60d22bc45e180a
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-13 13:30:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            int k = Integer.parseInt(stt.nextToken());
+            int a[] = new int[(int)(n)];
+            int b[] = new int[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            System.out.println(obj.kthElement( a, b, n, m, k));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        ArrayList<Integer> list=new ArrayList<>();
+        for(Integer ele:arr1){
+            list.add(ele);
+        }
+        for(Integer ele:arr2){
+            list.add(ele);
+        }
+        Collections.sort(list);
+        return list.get(k-1);
+        // int a=0
+        // if (n<m){
+        //     a=n;
+        // }
+        // else{
+        //     a=m;
+        // }
+        // for(int i=0;i<a;i++){
+        //     if (arr1[i]<arr[i])
+        // }
+        
+    }
+}
