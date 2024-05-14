0ad9e726ea89635e88057f4947e674825ea38782
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-14 11:27:09 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Reverse a String/reverse-a-string.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+class Solution:
+     def reverseWord(self, str: str) -> str:
+         return str[::-1]
+        #your code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while(t>0):
+        s = input()
+        ob = Solution()
+        print(ob.reverseWord(s))
+        t = t-1
+
+# } Driver Code Ends
