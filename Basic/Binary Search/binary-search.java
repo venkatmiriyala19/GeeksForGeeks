a4d3c76a0a65606bfea1809c9c580b1bd0acdb51
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-17 13:10:28 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Binary Search/binary-search.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+  public class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int key = sc.nextInt();
+            Solution g = new Solution();
+            System.out.println(g.binarysearch(arr, n, key));
+            T--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        int beg=0;
+        int end=n-1;
+        while(beg<=end){
+            int mid=beg+(end-beg)/2;
+            if(arr[mid]==k){
+                return mid;
+            }
+            else if(arr[mid]<k){
+                beg=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+        // code here
+    }
+}
