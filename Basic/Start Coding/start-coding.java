4b77b34d3fa2df094aa6699a9131760b17a50126
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-18 12:27:57 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Start Coding/start-coding.java
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            Solution obj = new Solution();
+            obj.printHello();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static void printHello() {
+        System.out.println("Hello World");
+        // code here
+    }
+}
+        
