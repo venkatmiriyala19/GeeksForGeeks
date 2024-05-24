e3b3f026e1ec20bf11ec911de6fbcce178c07686
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-24 13:39:32 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Pattern 6/pattern-6.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	    for(int i=n;i>=1;i--){
+	        for(int j=1;j<=i;j++){
+	            cout<<j<<" ";
+	        }
+	        cout<<endl;
+	    }
+	    return;
+	    // code here
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
