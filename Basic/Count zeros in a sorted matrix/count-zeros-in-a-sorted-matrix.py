8f3907183410f9735266e69bf52e05b50e7ceced
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-29 12:26:08 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Count zeros in a sorted matrix/count-zeros-in-a-sorted-matrix.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+class Solution:
+    def countZeroes(self, A, N):
+        count=0
+        for i in range(N):
+            for j in range(N):
+                if A[i][j]==0:
+                    count+=1
+        return count
+    
+    
+
+
+
+#{ 
+ # Driver Code Starts
+# Your code goes here
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input().strip())
+        arr = list(map(int, input().strip().split()))
+        matrix = [[0 for i in range(n)]for j in range(n)]
+        k=0
+        for i in range(n):
+            for j in range(n):
+                matrix[i][j] = arr[k]
+                k+=1
+        
+        ob = Solution()
+        print (ob.countZeroes(matrix, n))
+        
+# } Driver Code Ends
