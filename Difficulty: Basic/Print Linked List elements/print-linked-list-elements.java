1acf6315b15bf8b8ed71138c0186ac381203c8e8
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-07-11 11:11:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Linked List elements/print-linked-list-elements.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+
+
+public class linkedlist {
+
+    Node head;
+    Node tail ;
+    public void addToTheLast(Node node) {
+
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            
+            tail.next = node;
+            tail = tail.next ;
+        }
+    }
+ public static void main(String args[]) {
+
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            linkedlist llist = new linkedlist();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            llist.addToTheLast(head);
+                    
+            for (int i = 1; i < n; i++) {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }	
+            Solution ob=new Solution();
+            ob.display(llist.head);
+        System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* Node is defined as
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution
+{
+    // Print elements of a linked list on console
+    // head pointer input could be NULL as well
+    // for empty list
+    void display(Node head)
+    {
+        while(head!=null){
+            System.out.print(head.data+" ");
+            // System.out.print(" ");
+            head=head.next;
+        }
+        //add code here.
+    }
+}
