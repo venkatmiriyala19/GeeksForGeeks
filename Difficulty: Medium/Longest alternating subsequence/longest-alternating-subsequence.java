2b714aa7489a06bc6a1a23220fa45d9e7b306227
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-07-18 18:00:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest alternating subsequence/longest-alternating-subsequence.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+//Back-end complete function Template for Java
+class Solution {
+    public int alternatingMaxLength(int[] arr) {
+           int i=1;
+      int j=1;
+      for(int k=1; k<arr.length; k++)
+      {
+          if(arr[k]>arr[k-1])
+                i = j+1;
+            else if(arr[k]<arr[k-1])
+                j = i+1;
+      }
+      return Math.max(i,j);
+        // code here
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String s[] = br.readLine().trim().split(" ");
+            int[] nums = new int[s.length];
+            for (int i = 0; i < s.length; i++) {
+                nums[i] = Integer.parseInt(s[i]);
+            }
+            Solution obj = new Solution();
+            int ans = obj.alternatingMaxLength(nums);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
