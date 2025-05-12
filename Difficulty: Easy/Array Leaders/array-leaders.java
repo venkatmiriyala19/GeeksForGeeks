a01880fe3df6af4c5aa50a877de9e0da1bf0faf3
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2025-05-12 14:49:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            List<Integer> result = new Solution().leaders(arr);
+
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();    // Ensure new line after each test case output
+                System.out.println("~"); // Ensure new line after each test case output
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        // code here
+        int right=arr.length-1;
+        ArrayList<Integer> list=new ArrayList<>();
+        int max=0;
+        while(right>=0){
+            if(arr[right]>=max){
+                list.add(arr[right]);
+                max=arr[right];
+            }
+            right--;
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
