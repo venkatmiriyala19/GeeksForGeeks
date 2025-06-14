86bddef639573700afdf2e11b36186b30e8f0dd4
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2025-06-14 13:45:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.java
@@ -0,0 +1,25 @@
+// User function Template for Java
+
+class Solution {
+    static boolean isOperand(char c){
+        return (c>='a' && c<='z')|| (c>='A' && c<='Z') || (c>='0' && c<='9');
+    }
+    static String preToInfix(String exp) {
+        // code here
+        Stack<String> st=new Stack<>();
+        exp=new StringBuilder(exp).reverse().toString();
+        for(int i=0;i<exp.length();i++){
+            char c=exp.charAt(i);
+            if(isOperand(c)){
+                st.push(String.valueOf(c));
+            }else{
+                String t1=st.pop();
+                String t2=st.pop();
+                String ans="("+t1+c+t2+")";
+                st.push(ans);
+            }
+        }
+        return st.pop();
+        
+    }
+}
