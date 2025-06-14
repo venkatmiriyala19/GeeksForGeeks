6aea78a79019dfad2fa3156e1b2223656aed08e5
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2025-06-14 13:50:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Postfix to Prefix Conversion/postfix-to-prefix-conversion.java
@@ -0,0 +1,24 @@
+// User function Template for Java
+
+class Solution {
+    static boolean isOperand(char c){
+        return (c>='a' && c<='z')|| (c>='A' && c<='Z') || (c>='0' && c<='9');
+    }
+    static String postToPre(String exp) {
+        // code here
+        Stack<String> st=new Stack<>();
+        for(int i=0;i<exp.length();i++){
+            char c=exp.charAt(i);
+            if(isOperand(c)){
+                st.push(String.valueOf(c));
+            }
+            else{
+                String t1=st.pop();
+                String t2=st.pop();
+                String ans=c+t2+t1;
+                st.push(ans);
+            }
+        }
+        return st.pop();
+    }
+}
