d329e28e93585b42849e360f55ad6b910034a534
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2025-07-29 12:28:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
@@ -1,54 +1,32 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            int r = sc.nextInt();
-            int c = sc.nextInt();
-
-            int matrix[][] = new int[r][c];
-
-            for (int i = 0; i < r; i++) {
-                for (int j = 0; j < c; j++) matrix[i][j] = sc.nextInt();
-            }
-            Solution ob = new Solution();
-            ArrayList<Integer> ans = ob.spirallyTraverse(matrix);
-            for (Integer val : ans) System.out.print(val + " ");
-            System.out.println();
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to return a list of integers denoting spiral traversal of matrix.
-    public ArrayList<Integer> spirallyTraverse(int matrix[][]) {
+    public ArrayList<Integer> spirallyTraverse(int[][] mat) {
         // code here
-        ArrayList<Integer> ans=new ArrayList<>();
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int row=0,col=-1;
-        int direction=1;
-        while(n>0&&m>0){
-            for(int i=0;i<m;i++){
-                col+=direction;
-                ans.add(matrix[row][col]);
+        int n=mat.length;
+        int m=mat[0].length;
+        int left=0,right=m-1,top=0,bottom=n-1;
+        ArrayList<Integer> arr=new ArrayList<>();
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
+                arr.add(mat[top][i]);
             }
-            n--;
-            for(int i=0;i<n;i++){
-                row+=direction;
-                ans.add(matrix[row][col]);
+            top++;
+            for(int i=top;i<=bottom;i++){
+                arr.add(mat[i][right]);
             }
-            m--;
-            direction*=-1;
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    arr.add(mat[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    arr.add(mat[i][left]);
+                }
+                left++;
+            }
         }
-        return ans;
-        
+        return arr;
     }
 }
