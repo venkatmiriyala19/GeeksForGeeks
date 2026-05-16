c24b842116626f52f8f17ee3d5e42adb47116bd7
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2026-05-16 13:38:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
@@ -1,16 +1,16 @@
 class Solution {
-    public static void print_divisors(int n) {
+    public ArrayList<Integer> getDivisors(int n) {
         // code here
-        ArrayList<Integer> arr=new ArrayList<>();
-        for(int i=1;i*i<=n;i++){
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=1;i<=Math.sqrt(n);i++){
             if(n%i==0){
-                if(n/i!=i) arr.add(n/i);
-                System.out.print(i+" ");
+                list.add(i);
+                if(n/i!=i)
+                list.add(n/i);
             }
         }
-        for(int i=arr.size()-1;i>=0;i--){
-            System.out.print(arr.get(i)+" ");
-        }
-        // System.out.print(n);
+        Collections.sort(list);
+        // list.add(n);
+        return list;
     }
 }
