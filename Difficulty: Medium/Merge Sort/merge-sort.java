4e0198c10d855426595d5c70b55e15f713c9a476
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2026-05-25 14:37:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -2,38 +2,31 @@ class Solution {
 
     void mergeSort(int arr[], int l, int r) {
         // code here
-        if(l>=r){
-            return ;
-        }
+        if(l>=r) return;
         int mid=(l+r)/2;
         mergeSort(arr,l,mid);
         mergeSort(arr,mid+1,r);
         merge(arr,l,mid,r);
     }
-    void merge(int arr[],int l,int mid,int r){
-        ArrayList<Integer> temp=new ArrayList<>();
-        int left=l;
+    void merge(int arr[],int low,int mid,int high){
+        int left=low;
         int right=mid+1;
-        while(left<=mid && right<=r){
-            if(arr[left]<=arr[right]){
-                temp.add(arr[left]);
-                left++;
-            }
-            else{
-                temp.add(arr[right]);
-                right++;
+        ArrayList<Integer> temp=new ArrayList<>();
+        while(left<=mid && right<=high){
+            if(arr[left]<arr[right]){
+                temp.add(arr[left++]);
+            }else{
+                temp.add(arr[right++]);
             }
         }
         while(left<=mid){
-            temp.add(arr[left]);
-            left++;
+            temp.add(arr[left++]);
         }
-        while(right<=r){
-            temp.add(arr[right]);
-            right++;
+        while(right<=high){
+            temp.add(arr[right++]);
         }
-        for(int i=l;i<=r;i++){
-            arr[i]=temp.get(i-l);
+        for(int i=low;i<=high;i++){
+            arr[i]=temp.get(i-low);
         }
     }
 }
