5bb5a7c71a0fdd0d7e523a9691a7ac2c8f9c29d5
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2026-05-27 12:50:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.java
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -1,36 +1,31 @@
 class Solution {
     public void quickSort(int[] arr, int low, int high) {
         // code here
-        if(low<high){
-            int partitionIndex=partition(arr,low,high);
-            quickSort(arr,low,partitionIndex-1);
-            quickSort(arr,partitionIndex+1,high);
-        }
-        
+        if(low>=high) return;
+        int pi=partition(arr,low,high);
+        quickSort(arr,low,pi-1);
+        quickSort(arr,pi+1,high);
     }
 
     private int partition(int[] arr, int low, int high) {
-        int pivot=arr[high];
-        int i=low,j=high-1;
-        while(i<=j){
-            while( i<=j && arr[i]<=pivot ){
+        // code here
+        int pivot=arr[low];
+        int i=low,j=high;
+        while(i<j){
+            while( i<=j&&arr[i]<=pivot ){
                 i++;
             }
-            while(j>=i&& arr[j]>=pivot  ){
+            while( i<=j&&arr[j]>=pivot){
                 j--;
             }
             if(i<j){
                 int temp=arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
-                i++;j--;
             }
         }
-        int temp=arr[i];
-        arr[i]=pivot;
-        arr[high]=temp;
-        return i;
-        
-        // code here
+        arr[low]=arr[j];
+        arr[j]=pivot;
+        return j;
     }
 }
